914c804220639885d36b0de9730055a3435da504
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-09-05 13:35:36 +0300

    List component done

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import './List.scss';
 
 const List = ({ items }) => {
 
 
-    return (<ul className="todo__list">
+    return (<ul className="list">
         {
             items.map(item =>
-                <li className="active">
+                <li className={item.active ? 'active' : ''}>
                     <i>
-                        {item.icon}
+                        {item.icon ? (item.icon) : (<i className={`badge badge--${item.color}`}></i>)}
                     </i>
                     <span>{item.name}</span>
                 </li>)
         }
-
 
     </ul>);
 };
